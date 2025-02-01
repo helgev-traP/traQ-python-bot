1d2e49b17bf64f8425f727eccf35c1f15fb4507d
helgev <helgev22@gmail.com>
2025-02-01 12:20:24 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# install docker
 apt update
 apt install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings/
@@ -6,3 +7,7 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io -y
+
+# bui;d image and run
+docker build . -t bot-py
+# docker run --rm --privileged bot-py
